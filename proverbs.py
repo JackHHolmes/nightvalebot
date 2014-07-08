707b260b475cd81e430659f1bd62a3ff1b0e158a
Anthony <anthonysontag@gmail.com>
2014-07-08 15:05:15 -0500

    Update proverbs.py
    Fixed errors and duplicated the list so it is now listed under both functions.

--- proverbs.py
+++ proverbs.py
@@ -1,126 +1,244 @@
 
-import random.py
+import random
 
 def random_proverb():
-    pronum = randrange(1, 29)
+    pronum = random.randrange(1, 29)
 
-def proverb(n):
-    if n > 29:
-        proverb = 'No Proverb Found, Yet...'
-
-    elif (n == 1) or (pronum == 1):
+    if (pronum == 1):
         proverb = 'From Episode 1: Pilot "Look to the north. Keep looking. There\'s nothing coming from the south."'
 
-    elif (n == 2) or (pronum == 2):
+    elif (pronum == 2):
         proverb = 'From Episode 2: Glow Cloud "Men are from Mars; women are from Venus; Earth is a hallucination; podcasts are dreams."'
 
-    elif (n == 3) or (pronum == 3):
+    elif (pronum == 3):
         proverb = 'From Episode 3: Station Management "There\'s a special place in Hell. It\'s really hip. Very exclusive."'
 
-    elif (n == 4) or (pronum == 4):
+    elif (pronum == 4):
         proverb = 'From Episode 4: PTA Meeting "What has four legs in the morning, two legs at noon, and three legs in the evening? I don\'t know, but I trapped it in my bedroom. Send help."'
 
-    elif (n == 5) or (pronum == 5):
+    elif (pronum == 5):
         proverb = 'From Episode 5: The Shape in Grove Park "A million dollars isn\'t cool. You know what\'s cool? A basilisk."'
 
-    elif (n == 6) or (pronum == 6):
+    elif (pronum == 6):
         proverb = 'From Episode 6: The Drawbridge "Lost? Confused? Lacking direction? Need to find a purpose in your life?"'
 
-    elif (n == 7) or (pronum == 7):
+    elif (pronum == 7):
         proverb = 'From Episode 7: History Week "It must be 3:23 PM somewhere. Maybe space?"'
 
-    elif (n == 8) or (pronum == 8):
+    elif (pronum == 8):
         proverb = 'From Episode 8: The Lights in Radeon Canyon "We are living in an immaterial world, a ghost world, and I am an immaterial girl - a ghost."'
 
-    elif (n == 9) or (pronum == 9):
+    elif (pronum == 9):
         proverb = 'From Episode 9: "PYRAMID" ""Nice bolo tie," is the greatest compliment a person can ever receive."'
 
-    elif (n == 10) or (pronum == 10):
+    elif (pronum == 10):
         proverb = 'From Episode 10: Feral Dogs "Eating meat is a difficult moral decision, because it\'s stolen, that meat. You should apologise."'
 
-    elif (n == 11) or (pronum == 11):
+    elif (pronum == 11):
         proverb = 'From Episode 11: Wheat & Wheat By-Products "Today is the last day of your life up to this point."'
 
-    elif (n == 12) or (pronum == 12):
+    elif (pronum == 12):
         proverb = 'From Episode 12: The Canidate "Does the carpet match the drapes? No, it doesn\'t. You\'re the worst interior decorator. Please leave my home."'
 
-    elif (n == 13) or (pronum == 13):
+    elif (pronum == 13):
         proverb = 'From Episode 13: A Story About You "I\'d never join a PEN15 club that would allow a person like me to become a member."'
 
-    elif (n == 14) or (pronum == 14):
+    elif (pronum == 14):
         proverb = 'From Episode 14: The Man in the Tan Jacket "Biologically speaking, we are all people made up of smaller people."'
 
-    elif (n == 15) or (pronum == 15):
+    elif (pronum == 15):
         proverb = 'From Episode 15: Street Cleaning Day "One incorporeal being said to the other, "I\'m not here too! Make friends?""'
 
-    elif (n == 16) or (pronum == 16):
+    elif (pronum == 16):
         proverb = 'From Episode 16: The Phone Call "If I said you had a beautiful body, would it even matter because we are so insignificant in this vast incomprehensible universe?"'
 
-    elif (n == 17) or (pronum == 17):
+    elif (pronum == 17):
         proverb = 'From Episode 17: Valentine "Werner Herzog is the most interesting person."'
 
-    elif (n == 18) or (pronum == 18):
+    elif (pronum == 18):
         proverb = 'From Episode 18: The Traveler "Find more ways to work \'plinth\' into daily conversations."'
 
-    elif (n == 19) or (pronum == 19):
+    elif (pronum == 19):
         proverb = 'From Episode 19A: The Sandstorm (Part A) "Step one: write down the names of everyone you know. Step two: rearrange the letters. Step three: this will reveal a great secret of time." From Episode 19B: The Sandstorm (Part B) "Step one: seperate your lips. Step two: use facial muscles to pull back corners of mouth. Step three: widen your eyes. This is how to be happy."'
 
-    elif (n == 20) or (pronum == 20):
+    elif (pronum == 20):
         proverb = 'From Episode 20: Poetry Week "Pain is just weakness leaving the body, and then being replaced by pain. Lots of pain."'
 
-    elif (n == 21) or (pronum == 21):
+    elif (pronum == 21):
         proverb = 'From Episode 21: A Memory of Europe "sk your doctor if right is left for you."'
 
-    elif (n == 22) or (pronum == 22):
+    elif (pronum == 22):
         proverb = 'From Episode 22: The Whispering Forest "If you love someone, set them free. Set them free now. This is the police, and we have you surrounded."'
 
-    elif (n == 23) or (pronum == 23):
+    elif (pronum == 23):
         proverb = 'From Episode 23: Eternal Scouts "Mamas, don\'t let your babies grow up to be cowboys. Show them pictures of cows when they\'re young and administer brief electrical shocks."'
 
     elif (n == 24) or (pronum == 24):
         proverb = 'From Episode 24: The Mayor "The most dangerous game is man. The most entertaining game is Broadway Puppy Ball. The most weird game is Esoteric Bear."'
 
-    elif (n == 25) or (pronum == 25):
+    elif (pronum == 25):
         proverb = 'From Episode 25: One Year Later "Fun game. Say "toy boat" over and over. Do it for the rest of your life. Retreat from society and live on alms. Whisper "toy boat" as you die."'
 
-    elif (n == 26) or (pronum == 26):
+    elif (pronum == 26):
         proverb = 'From Episode 26: Faceless Old Woman "The human soul weighs 21 grams, smells like grilled vegetables, looks like a wrinkled tartan quilt, and sounds like bridge traffic."'
 
-    elif (n == 27) or (pronum == 27):
+    elif (pronum == 27):
         proverb = 'From Episode 27: First Date "Production oversight by Tory Malatea, who is holding a small locket. He is not speaking. He\'d just like for you to touch the locket. His hand is twisted. His skin is forming into scales. Just touch it once. Just once, okay?"'
 
-    elif (n == 28) or (pronum == 28):
+    elif (pronum == 28):
         proverb = 'From Episode 28: Summer Reading Program "A bar walks into a bar. The bartender is a snake eating its own tail. The windows look out only onto the face of the one who looks."'
 
     elif (n == 29) or (pronum == 29):
         proverb = 'From Episode 29: Subway "Your body is a temple. A temple of blood rituals and pagan tributes, a lost temple, a temple that needs more calcium. You should maybe try vitamin supplements."'
 
-    elif (n == 30) or (pronum == 30):
+    elif (pronum == 30):
         proverb = 'From Episode 30:  ""'
 
-    elif (n == 31) or (pronum == 32):
+    elif (pronum == 32):
         proverb = 'From Episode :  ""'
 
-    elif (n == 32) or (pronum == 32):
+    elif (pronum == 32):
         proverb = 'From Episode :  ""'
 
-    elif (n == 33) or (pronum == 33):
+    elif (pronum == 33):
         proverb = 'From Episode :  ""'
 
-    elif (n == 34) or (pronum == 34):
+    elif (pronum == 34):
         proverb = 'From Episode :  ""'
 
-    elif (n == 35) or (pronum == 35):
+    elif (pronum == 35):
         proverb = 'From Episode :  ""'
 
-    elif (n == 36) or (pronum == 36):
+    elif (pronum == 36):
         proverb = 'From Episode :  ""'
 
-    elif (n == 37) or (pronum == 37):
+    elif (pronum == 37):
         proverb = 'From Episode :  ""'
 
-    elif (n == 38) or (pronum == 38):
+    elif (pronum == 38):
         proverb = 'From Episode :  ""'
 
-    elif (n == 39) or (pronum == 39):
+    elif (pronum == 39):
+        proverb = 'From Episode :  ""'
+
+
+def proverb(n):
+    if n > 29:
+        proverb = 'No Proverb Found, Yet...'
+
+    elif (n == 1):
+        proverb = 'From Episode 1: Pilot "Look to the north. Keep looking. There\'s nothing coming from the south."'
+
+    elif (n == 2):
+        proverb = 'From Episode 2: Glow Cloud "Men are from Mars; women are from Venus; Earth is a hallucination; podcasts are dreams."'
+
+    elif (n == 3):
+        proverb = 'From Episode 3: Station Management "There\'s a special place in Hell. It\'s really hip. Very exclusive."'
+
+    elif (n == 4):
+        proverb = 'From Episode 4: PTA Meeting "What has four legs in the morning, two legs at noon, and three legs in the evening? I don\'t know, but I trapped it in my bedroom. Send help."'
+
+    elif (n == 5):
+        proverb = 'From Episode 5: The Shape in Grove Park "A million dollars isn\'t cool. You know what\'s cool? A basilisk."'
+
+    elif (n == 6):
+        proverb = 'From Episode 6: The Drawbridge "Lost? Confused? Lacking direction? Need to find a purpose in your life?"'
+
+    elif (n == 7):
+        proverb = 'From Episode 7: History Week "It must be 3:23 PM somewhere. Maybe space?"'
+
+    elif (n == 8):
+        proverb = 'From Episode 8: The Lights in Radeon Canyon "We are living in an immaterial world, a ghost world, and I am an immaterial girl - a ghost."'
+
+    elif (n == 9):
+        proverb = 'From Episode 9: "PYRAMID" ""Nice bolo tie," is the greatest compliment a person can ever receive."'
+
+    elif (n == 10):
+        proverb = 'From Episode 10: Feral Dogs "Eating meat is a difficult moral decision, because it\'s stolen, that meat. You should apologise."'
+
+    elif (n == 11):
+        proverb = 'From Episode 11: Wheat & Wheat By-Products "Today is the last day of your life up to this point."'
+
+    elif (n == 12):
+        proverb = 'From Episode 12: The Canidate "Does the carpet match the drapes? No, it doesn\'t. You\'re the worst interior decorator. Please leave my home."'
+
+    elif (n == 13):
+        proverb = 'From Episode 13: A Story About You "I\'d never join a PEN15 club that would allow a person like me to become a member."'
+
+    elif (n == 14):
+        proverb = 'From Episode 14: The Man in the Tan Jacket "Biologically speaking, we are all people made up of smaller people."'
+
+    elif (n == 15):
+        proverb = 'From Episode 15: Street Cleaning Day "One incorporeal being said to the other, "I\'m not here too! Make friends?""'
+
+    elif (n == 16):
+        proverb = 'From Episode 16: The Phone Call "If I said you had a beautiful body, would it even matter because we are so insignificant in this vast incomprehensible universe?"'
+
+    elif (n == 17):
+        proverb = 'From Episode 17: Valentine "Werner Herzog is the most interesting person."'
+
+    elif (n == 18):
+        proverb = 'From Episode 18: The Traveler "Find more ways to work \'plinth\' into daily conversations."'
+
+    elif (n == 19):
+        proverb = 'From Episode 19A: The Sandstorm (Part A) "Step one: write down the names of everyone you know. Step two: rearrange the letters. Step three: this will reveal a great secret of time." From Episode 19B: The Sandstorm (Part B) "Step one: seperate your lips. Step two: use facial muscles to pull back corners of mouth. Step three: widen your eyes. This is how to be happy."'
+
+    elif (n == 20):
+        proverb = 'From Episode 20: Poetry Week "Pain is just weakness leaving the body, and then being replaced by pain. Lots of pain."'
+
+    elif (n == 21):
+        proverb = 'From Episode 21: A Memory of Europe "sk your doctor if right is left for you."'
+
+    elif (n == 22):
+        proverb = 'From Episode 22: The Whispering Forest "If you love someone, set them free. Set them free now. This is the police, and we have you surrounded."'
+
+    elif (n == 23):
+        proverb = 'From Episode 23: Eternal Scouts "Mamas, don\'t let your babies grow up to be cowboys. Show them pictures of cows when they\'re young and administer brief electrical shocks."'
+
+    elif (n == 24):
+        proverb = 'From Episode 24: The Mayor "The most dangerous game is man. The most entertaining game is Broadway Puppy Ball. The most weird game is Esoteric Bear."'
+
+    elif (n == 25):
+        proverb = 'From Episode 25: One Year Later "Fun game. Say "toy boat" over and over. Do it for the rest of your life. Retreat from society and live on alms. Whisper "toy boat" as you die."'
+
+    elif (n == 26):
+        proverb = 'From Episode 26: Faceless Old Woman "The human soul weighs 21 grams, smells like grilled vegetables, looks like a wrinkled tartan quilt, and sounds like bridge traffic."'
+
+    elif (n == 27):
+        proverb = 'From Episode 27: First Date "Production oversight by Tory Malatea, who is holding a small locket. He is not speaking. He\'d just like for you to touch the locket. His hand is twisted. His skin is forming into scales. Just touch it once. Just once, okay?"'
+
+    elif (n == 28):
+        proverb = 'From Episode 28: Summer Reading Program "A bar walks into a bar. The bartender is a snake eating its own tail. The windows look out only onto the face of the one who looks."'
+
+    elif (n == 29):
+        proverb = 'From Episode 29: Subway "Your body is a temple. A temple of blood rituals and pagan tributes, a lost temple, a temple that needs more calcium. You should maybe try vitamin supplements."'
+
+    elif (n == 30):
+        proverb = 'From Episode 30:  ""'
+
+    elif (n == 31):
+        proverb = 'From Episode :  ""'
+
+    elif (n == 32):
+        proverb = 'From Episode :  ""'
+
+    elif (n == 33):
+        proverb = 'From Episode :  ""'
+
+    elif (n == 34):
+        proverb = 'From Episode :  ""'
+
+    elif (n == 35):
+        proverb = 'From Episode :  ""'
+
+    elif (n == 36):
+        proverb = 'From Episode :  ""'
+
+    elif (n == 37):
+        proverb = 'From Episode :  ""'
+
+    elif (n == 38):
+        proverb = 'From Episode :  ""'
+
+    elif (n == 39):
         proverb = 'From Episode :  ""'
