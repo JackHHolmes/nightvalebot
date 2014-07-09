dac50aa78c1fdff24825db3189f1fc2011f8e532
Anthony <anthonysontag@gmail.com>
2014-07-09 14:09:08 -0500

    Create character.py
    A function that picks a random character from Night Vale and gives their name, epitaph, and an action or detail about them.

---
+++ character.py
@@ -0,0 +1,24 @@
+import random
+
+def char_prof():
+	number = random.raindit(1,10)
+	if number == int('1'):
+		 profile = 'Cecil Gershwin Palmer, the Welcome to Night Vale radio host, hates Steve Carlsberg '
+	if number == int('2'):
+		profile = 'Carlos, the scientist, is investigating why time stopped again.'
+	if number == int('3'):
+		profile = 'The Faceless Old Woman, the one that secretly lives in your home, set your fridge on fire.'
+	if number == int('4'):
+		profile = 'Hiram McDaniels, the five-headed dragon, is still at large for insurance fraud and exscaping police custody.'
+	if number == int('5'):
+		profile = 'Steve Carlsberg, STEVE CARLSBERG! He is just the worst.'
+	if number == int('6'):
+		char_prof = 'Kevin, the Desert Bluffs radio host, has a gore-filled broadcasting booth.'
+	if number == int('7'):
+		char_prof = 'Khoshekh, the floating cat in the radio station bathroom, has given birth to kittens.'
+	if number == int('8'):
+		char_prof = 'Dana Caridinal, former Night Vale radio station intern, has been trapped in the Dog Park.'
+	if number == int('9'):
+		char_prof = 'Tamika Flynn, the destroyer of librarians, has an army of child soldiers.'
+	if number == int('10'):
+		char_prof = 'Pamela Winchell, the mayor of Night Vale, is holding a press conference in the middle of the woods and attended by animals.'
